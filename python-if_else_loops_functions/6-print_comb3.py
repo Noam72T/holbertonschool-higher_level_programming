d4b28552d4b0f4d14986fe3a0ea6794c4c82e007
Noam72T <9823@holbertonstudents.com>
2025-01-15 14:05:48 +0000

    Print comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-print(", ".join("{:d}{:d}".format(i, j) for i in range(10) for j in range(i + 1, 10)))
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i < 8 or (i == 8 and j < 9):
+            print("{}{},".format(i, j), end=" ")
+        else:
+            print("{}{}".format(i, j))
+            
