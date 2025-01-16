edc9cf0f5c904c35b5407e48e5c9c695de17c411
Noam72T <9823@holbertonstudents.com>
2025-01-16 13:15:22 +0100

    Addition

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
 
+    from add_0 import add
+
+    a = 1
+    b = 2
     result = add(a, b)
-    result1 = sub(a, b)
-    result2 = mul(a, b)
-    result3 = div(a, b)
 
     print("{} + {} = {}".format(a, b, result))
-    print("{} - {} = {}".format(a, b, result1))
-    print("{} * {} = {}".format(a, b, result2))
-    print("{} / {} = {}".format(a, b, result3))
+    
