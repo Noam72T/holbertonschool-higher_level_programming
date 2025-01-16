1a067fc43e1c8ff6780c4e1bb6480fa1c8f65c23
Noam72T <9823@holbertonstudents.com>
2025-01-16 11:09:48 +0100

    Args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == '__main__':
+    import sys
     """Prints the argument list passed to the program
 
     The program takes all the arguments starting from the second
