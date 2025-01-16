193b8f88358b93c3c267f3619c69ba66bf61f4b2
Noam72T <9823@holbertonstudents.com>
2025-01-16 14:15:35 +0100

    Fix Argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == '__main__':
-    """Prints the argument list passed to the program
-
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-
-    """
-    av = sys.argv
-    l_av = len(av) - 1
-
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+    for i, arg in enumerate(args, 1):
+        print("{}: {}".format(i, arg))
+        
