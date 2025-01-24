5deb032a5f055ad2a8896d275f07c6854874b8df
Noam72T <9823@holbertonstudents.com>
2025-01-24 09:24:22 +0000

    Fix Safe

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        # Essaye de formater et d'imprimer la valeur comme un entier
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        # Si une erreur survient (par ex., si ce n'est pas un entier), retourne False
         return False
+    
