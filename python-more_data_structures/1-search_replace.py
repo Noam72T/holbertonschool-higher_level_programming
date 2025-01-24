41aee79f7c7e39091bcc1a5eabdb532069878fd9
Noam72T <9823@holbertonstudents.com>
2025-01-24 10:09:11 +0000

    Fix Pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,4 @@ def search_replace(my_list, search, replace):
     """
     Replaces all occurrences of an element by another in a new list.
     """
-    
     return [replace if element == search else element for element in my_list]
