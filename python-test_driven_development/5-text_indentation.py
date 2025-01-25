a294276bc425571b390490a7890891bb510a7f4d
Noam72T <9823@holbertonstudents.com>
2025-01-25 22:30:20 +0000

    Fix all

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,18 +7,23 @@ This module contains a function that indents text according to specific rules.
 
 def text_indentation(text):
     """
-    Function that prints a text with two new lines after each
-    of these characters:
+    Function that prints a text with two new lines
+    after each of these characters:
+    `.`, `?`, `:`.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     i = 0
     while i < len(text):
+        # Check if the current character is punctuation and print a newline
         if text[i] in '.?:':
             print(text[i], end="\n\n")
             i += 1
+            # Skip all spaces after the punctuation mark
             while i < len(text) and text[i] == " ":
                 i += 1
         else:
+            # Print the current character if it's not punctuation
             print(text[i], end="")
             i += 1
