f3796278a945b8fab3c618e7fd0c1ab743a05bd6
Elisea 76 <9823@holbertonstudents.com>
2025-01-26 23:32:54 +0100

    Fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,13 +5,16 @@ This module contains a function that
 prints My name is <first name> <last name>.
 """
 
-
 def say_my_name(first_name, last_name=""):
     """
-    Function that prints
+    Function that prints the name in the format:
+    'My name is <first_name> <last_name>'
+    Raises TypeError if input is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name).strip())
+
+    # Print the name without adding extra space after the last name
+    print("My name is {} {}".format(first_name, last_name).rstrip())
