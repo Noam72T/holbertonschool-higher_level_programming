4a6586a98ee0895b9a45c98e2c87522ffe98b285
Elisea 76 <9823@holbertonstudents.com>
2025-01-27 10:51:13 +0100

    Create Private sqaure

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines a class square
+"""
+
+
+class Sqaure:
+    """
+    Represent a square.
+
+    Attributes:
+    __size (int): The size of the square
+    """
+
+    def __init__(self, size):
+        self.__size = size
