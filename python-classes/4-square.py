eb8d0198422d19f49bbd108e38a138e5778be7e9
Elisea 76 <9823@holbertonstudents.com>
2025-01-28 09:29:11 +0100

    Push Project

---
+++ python-classes/4-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Module 4-square
+Defines a class Square that represents a square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
