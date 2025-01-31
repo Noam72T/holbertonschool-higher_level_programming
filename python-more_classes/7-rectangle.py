4d166051328da90c10ff020f50f2dd9ce03550eb
Noam72T <9823@holbertonstudents.com>
2025-01-31 17:18:50 +0100

    Fix Pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -60,7 +60,8 @@ class Rectangle:
         the rectangle with print_symbol."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        return "\n".join([str(self.print_symbol) *
+                          self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """Return a string representation of
