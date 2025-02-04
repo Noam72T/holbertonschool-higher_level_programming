ff3197e99a2e81d9200ae3e5a8a1ad591e44ed97
Noam72T <9823@holbertonstudents.com>
2025-02-04 09:32:34 +0100

    Files

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class MyList(list):
     """
     A subclass of list that includes a method to print the list in sorted order.
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def inherits_from(obj, a_class):
     """
     Checks if an object is an instance of a class that inherited (directly or indirectly) from the specified class.
