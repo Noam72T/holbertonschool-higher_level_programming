fe35336f32ce859bc216510f9ff925b1a8606ea6
Noam72T <9823@holbertonstudents.com>
2025-02-05 09:35:47 +0100

    Fix Pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
 
     Args:
-        obj (any): The object to check.
+        obj (any): The object to checks.
         a_class (type): The class to match the type of obj to.
     Returns:
         If obj is an inherited instance of a_class - True.
