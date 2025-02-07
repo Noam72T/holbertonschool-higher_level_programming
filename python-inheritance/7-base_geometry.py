7ac1d81a90847bdaf2812c720646004b36b26038
Noam72T <9823@holbertonstudents.com>
2025-02-07 14:37:43 +0100

    Fix correction

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module - 7-base_geometry"""
+'''Mode - 7-base_geometry'''
 
 
 class BaseGeometry:
-    """Base Geometry class"""
+    '''Base Geometry class'''
 
-    Def area(self):
+    def area(self):
         """Not Implémented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """functionn fir int validator"""
+        '''fnc fir int validator'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
