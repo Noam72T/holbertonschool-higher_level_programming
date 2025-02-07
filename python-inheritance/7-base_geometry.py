6e8ea8b903ad7f4653399167a473c272ed6e6b09
Noam72T <9823@holbertonstudents.com>
2025-02-07 14:34:42 +0100

    Fix correction

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-'''Module - 7-base_geometry: Définit une classe de base pour la géométrie.''' 
+'''Mode - 7-base_geometry'''
+
 
 class BaseGeometry:
-    '''Classe de base pour la géométrie, servant de modèle pour d'autres formes géométriques.'''
+    '''Base Geometry class'''
 
     def area(self):
-        """Méthode qui lève une exception car elle n'est pas implémentée.
-        Elle doit être redéfinie dans une sous-classe pour fournir une implémentation spécifique.
-        """
+        """Not Implémented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        '''
-        Méthode qui valide si un paramètre est un entier strictement positif.
-        
-        Arguments:
-        name (str): Le nom du paramètre (utilisé dans le message d'erreur).
-        value (int): La valeur à vérifier.
-        
-        Exceptions:
-        - TypeError: Levée si `value` n'est pas un entier.
-        - ValueError: Levée si `value` est inférieur ou égal à 0.
-        '''
+        '''fnc fir int validator'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
