96994709e8db7570b86f4bcd8d1d4ea415cae381
Noam72T <9823@holbertonstudents.com>
2025-02-11 13:01:08 +0100

    Fix task

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''Function to Append File'''
 
+
 def append_write(filename="", text=""):
-    """Add string and return number."""
+    """Add string and return number"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
