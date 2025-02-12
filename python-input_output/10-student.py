21bebece7b2e528c8b71b1543ddf981506553283
Noam72T <9823@holbertonstudents.com>
2025-02-12 09:58:17 +0100

    Add task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 '''Class Student'''
 
-
 class Student:
     def __init__(self, first_name, last_name, age):
-        """
-        Initialise une nouvelle instance de Student.
-
-        Args:
-            first_name (str): Name of the student.
-            last_name (str): Last name of the student.
-            age (int): Age of the student
-        """
+        """Initialize the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Return a JSON representation of the instance.
+        """Return the dictionary description"""
+        class_d = self.__dict__
+        sel_d = dict()
 
-        Args:
-            attrs (list): List of attributes to	return.
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_d
 
-        Returns:
-            dict: Dictionnary.
-        """
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+                if attr in class_d:
+                    sel_d[attr] = class_d[attr]
+
+            return sel_d
+
+        return class_d
