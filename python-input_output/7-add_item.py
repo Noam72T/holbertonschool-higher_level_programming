5bd28a9c21eb43d6bcd0fefc027b6003d354e603
Noam72T <9823@holbertonstudents.com>
2025-02-12 09:27:30 +0100

    Fix task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+'''Add item to JSON file'''
 from os import path
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
+filename = 'add_item.json'
+
+
+if path.exists(filename):
+    obj_json_file = load_from_json_file(filename)
 else:
     obj_json_file = []
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
 
-save_to_json_file(obj_json_file, 'add_item.json')
+obj_json_file.extend(argv[1:])
+
+
+save_to_json_file(obj_json_file, filename)
