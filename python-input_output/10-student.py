674dcf9460aa5a6ebafd0edc3ddb16bf2c48c1b5
Noam72T <9823@holbertonstudents.com>
2025-02-12 10:12:14 +0100

    fix 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-'''Class Student'''
+"""Class Student"""
 
 
 class Student:
-    """Student"""
-
     def __init__(self, first_name, last_name, age):
-        """
-        Initialise une nouvelle instance de Student.
-
-        Args:
-            first_name (str): Name of the student.
-            last_name (str): Last name of the student.
-            age (int): Age of the student
-        """
+        """Public instance attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Return a JSON representation of the instance.
-
-        Args:
-            attrs (list): List of attributes to	return.
-
-        Returns:
-            dict: Dictionnary.
-        """
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        """return  dictionary representation of a Student"""
+        if isinstance(attrs, list):
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
+        return self.__dict__
