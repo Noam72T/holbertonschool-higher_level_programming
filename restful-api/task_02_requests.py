5b93c114014265677ec2bb1f6926a06975777c1d
Noam72T <9823@holbertonstudents.com>
2025-02-17 11:09:52 +0100

    Task 02 and Readme

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,32 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+    else:
+        print("Failed to fetch posts")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            for post in posts:
+                writer.writerow({"id": post["id"], "title": post["title"], "body": post["body"]})
+        print("Posts saved to posts.csv")
+    else:
+        print("Failed to fetch posts")
