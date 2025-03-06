a6283cd345ae0592b7ae56f6bc1632e33692fec9
Noam72T <9823@holbertonstudents.com>
2025-03-06 15:00:54 +0100

    Filter States fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""Module listing all states matching a name from the database"""
-
+"""Module listing all Arizona name from the database"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Connexion à la base de données
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cur = db.cursor()
-
-    # Exécution de la requête avec un paramètre de manière sécurisée
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC",
-        (argv[4],)
-    )
-
-    # Récupération et affichage des résultats
+        """SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"""
+        .format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
-
-    # Fermeture du curseur et de la connexion
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
