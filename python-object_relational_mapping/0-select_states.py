159c968910d160a11c0ebafe89d1235fb4f7a54f
Noam72T <9823@holbertonstudents.com>
2025-03-06 12:52:53 +0100

    Fix Files

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,7 @@
 import MySQLdb
 import sys
 
-
-def list_states():
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
@@ -13,7 +12,3 @@ def list_states():
         print(row)
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    list_states()
