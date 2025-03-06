0fa8f5a6de345ba569725ff1e670b5782a11506e
Noam72T <9823@holbertonstudents.com>
2025-03-06 12:38:23 +0100

    Fix output

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
 import sys
+import argv
 import MySQLdb
-"""Function List states"""
+"""Function List all states"""
 
+if __name__ == '__main__':
+    """Connection to database"""
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306
+                         passwd=argv[2], db=argv[3])
 
-def list_states():
-    # Get arguments from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    # Establish the MySQL connection
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-    # Create a cursor object
-    cursor = db.cursor()
-    # Execute the query to get all states in ascending order by id
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-    # Fetch all rows from the executed query
-    states = cursor.fetchall()
-    # Print the results
-    for state in states:
-        print(state)
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchAll()
 
-
-if __name__ == "__main__":
-    list_states()
+    for row in rows:
+        print(row)
