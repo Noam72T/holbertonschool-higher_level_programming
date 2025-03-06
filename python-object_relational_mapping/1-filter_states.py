ded0d2b00efdd92c5216b445ec28aa21a7b92932
Noam72T <9823@holbertonstudents.com>
2025-03-06 13:52:13 +0100

    fix1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,8 +2,7 @@
 import MySQLdb
 import sys
 
-
-def list_states():
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
@@ -16,8 +15,3 @@ def list_states():
         print(row)
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-
-    list_states()
