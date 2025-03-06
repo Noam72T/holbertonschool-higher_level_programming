eb76b1b3509eeac10ddffe213fc4b07d5a0a18e6
Noam72T <9823@holbertonstudents.com>
2025-03-06 11:56:22 +0100

    Fix select stattes with Pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 import sys
 import MySQLdb
+"""Function List states"""
+
 
 def list_states():
     # Get arguments from command line
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
     # Establish the MySQL connection
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
     # Create a cursor object
     cursor = db.cursor()
-
     # Execute the query to get all states in ascending order by id
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-
     # Fetch all rows from the executed query
     states = cursor.fetchall()
-
     # Print the results
     for state in states:
         print(state)
-
     # Close the cursor and connection
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     list_states()
